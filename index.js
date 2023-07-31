1af02254ce0b433827e597134ff9d2eaedc07d1d
pellegabriel <81648480+pellegabriel@users.noreply.github.com>
2023-07-31 10:53:11 -0300

    firebaseConfig en index.js
    rellenar con la configuracion de firebase correspondiente

--- index.js
+++ index.js
@@ -2,8 +2,24 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import firebase from '@react-native-firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "myapp-project-123.firebaseapp.com",
+  databaseURL: "https://myapp-project-123.firebaseio.com",
+  projectId: "myapp-project-123",
+  storageBucket: "myapp-project-123.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123:web:ee873bd1234baba4ce549",
+};
+
+// Inicializa Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 AppRegistry.registerComponent(appName, () => App);
